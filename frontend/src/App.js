af16a1aab194c73f94f0c27fdd1d5c11c3d78393
JehSA <jef.almeida@gmail.com>
2024-04-10 23:19:48 -0300

    HTML e CSS prontos.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,45 @@
 import logo from './logo.svg';
+import {AiOutlineEdit, AiOutlineDelete} from 'react-icons/ai';
 import './App.css';
+
+const arrayTodos = [
+  { name: "Limpar a casa", status: true},
+  { name: "Banho no cachorro", status: false}
+
+];
+
+const Todos = ({ todos }) => {
+  return (
+    <div className="todos">
+      {todos.map(todo => {
+        return (
+          <div className="todo">
+            <button className='checkbox' style={{backgroundColor: todo.status ? "#A879E6" : "#FFFFFF"}}></button>
+            <p>{ todo.name }</p>
+            <button>
+              <AiOutlineEdit size={20} color={"#64697b"}></AiOutlineEdit>
+            </button>
+            <button>
+              <AiOutlineDelete size={20} color={"#64697b"}></AiOutlineDelete>
+            </button>
+          </div>
+        );
+      })};
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="container">
-        <div className="todosDiv">
-          <p>TESTE JEH</p>
+        <div className='header'>
+          <h1>Dont be lazzy</h1>
         </div>
-      </header>
+        <Todos todos={arrayTodos}></Todos>
+        <input className='imputName'></input>
+        <button className='newTaskButton'>+ Newtask</button>
+      </header>      
     </div>
   );
 }
